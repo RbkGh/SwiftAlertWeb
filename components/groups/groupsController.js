bff3bb2e7c47099d32051d596675ca180b8575be
Jahlom Precious Agboado <Jahlom Precious Agboado>
2016-10-10 21:28:12 +0000

    fix

--- components/groups/groupsController.js
+++ components/groups/groupsController.js
@@ -1,7 +1,28 @@
 angular.module('swiftAlert')
     .controller('groupsController', ['$location', 'Auth', 'Groups', '$localStorage', function($location, Auth, Groups, $localStorage) {
-        var sett = this;
-        Groups.getGroups($localStorage.currentUser.userName,function (result) {
-            console.log(result);
-        });
+        var grp = this;
+        initController();
+        grp.editGroup = editGroup;
+        grp.deleteGroup = deleteGroup;
+
+        function initController() {
+            Groups.getGroups($localStorage.currentUser.userName)
+                .then(function (groups) {
+                   grp.groups = groups;
+                })
+                .catch(function(response) {
+                    console.error('Error', response.status, response.data);
+                });
+        };
+
+        function editGroup(groupId, groupName) {
+            console.log(groupId);
+            grp.groupToBeEditeddName = groupName;
+            $('#editGroup').modal('show');
+        };
+
+        function deleteGroup(groupId, groupName) {
+            grp.groupToBeDeletedName = groupName;
+            $('#deleteGroup').modal('show');
+        };
     }]);
