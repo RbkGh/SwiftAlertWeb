c8cc67a7816907166bc6c38acddeb88e8159bf67
Jahlom Precious Agboado <Jahlom Precious Agboado>
2016-10-10 21:28:50 +0000

    fix

--- services/groups/Groups.js
+++ services/groups/Groups.js
@@ -21,47 +21,12 @@
                       data:{}
               }).then(
                   function success(response) {
-                    console.log(response);
                       return response.data.responseObject;
                   },
                   function fail(e) {
                     console.log(e);
                       return e;
               });
-            // $.get( ROOT+'/api/v2/groups/'+username, function( response ) {
-            //     if (response) {
-            //         console.log(response);
-            //         callback(response);
-            //     } else {
-            //         callback(false);
-            //     }
-            // });
-            // var settings = {
-            //   "async": true,
-            //   "crossDomain": true,
-            //   "url": "http://212.111.42.10:8080/swiftalertmain/api/v2/groups/"+username,
-            //   "method": "GET",
-            //   "headers": {
-            //     "authorization": 'Bearer ' + $localStorage.currentUser.token,
-            //     "content-type": "application/x-www-form-urlencoded"
-            //   }
-            // }
-            //
-            // $.ajax(settings).done(function (response) {
-            //   callback(response.responseObject);
-            // });
-
-            // console.log($http.defaults.headers);
-            // $http.defaults.headers.get["Content-Type"] = "application/x-www-form-urlencoded;charset=utf-8;";
-            // $http.get(ROOT+'/api/v2/groups/'+username, {headers:{'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}})
-            //     .success(function (response) {
-            //         if (response) {
-            //             console.log(response);
-            //             callback(response);
-            //         } else {
-            //             callback(false);
-            //         }
-            //     });
         }
     }
 })();
