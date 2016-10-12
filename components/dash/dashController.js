5342cdfedd792c21acccb78e0c2d9930fde2b1ff
Jahlom Precious Agboado <Jahlom Precious Agboado>
2016-10-12 21:53:34 +0000

    added credit

--- components/dash/dashController.js
+++ components/dash/dashController.js
@@ -1,9 +1,19 @@
 angular.module('swiftAlert')
-    .controller('dashController', ['$location', 'Auth', 'Groups', '$localStorage', function($location, Auth, Groups, $localStorage) {
+    .controller('dashController', ['$location', 'Auth', 'User', '$localStorage', function($location, Auth, User, $localStorage) {
         var dash = this;
-        
-        dash.logout = logout;
 
+        dash.logout = logout;
+        init();
+
+        function init() {
+            User.get()
+                .then(function (userinfo) {
+                    dash.userinfo = userinfo.responseObject;
+                })
+                .catch(function (response) {
+                    console.error('Error', response.status, response.data);
+                });
+        }
         function logout() {
             Auth.Logout();
             $location.path('/login');
--- services/user/User.js
+++ services/user/User.js
@@ -5,19 +5,35 @@
         .module('swiftAlert')
         .factory('User', User);
 
-    function User($http) {
+    function User($http, $localStorage) {
         var service = {};
-        var ROOT = "http://212.111.42.10:8080/swiftalertmain/api/v2"
+        var ROOT = "http://212.111.42.10:8080/swiftalertmain/api/v2/user"
 
+        service.get = get;
         service.changePassword = changePassword;
 
         return service;
 
+        function get() {
+            return $http({
+                        method: 'GET',
+                        url: ROOT+'/'+$localStorage.currentUser.userId,
+                        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                        data: {}
+                }).then(
+                    function success(response) {
+                        return response.data;
+                    },
+                    function fail(e) {
+                      console.log(e);
+                        return e;
+                });
+        }
 
         function changePassword(data) {
             return $http({
                         method: 'PUT',
-                        url: ROOT+'/user/passwordchange',
+                        url: ROOT+'/passwordchange',
                         headers: {'Content-Type': 'application/json;charset=UTF-8'},
                         data: data
                 }).then(
